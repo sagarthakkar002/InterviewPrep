c454808e816f0c4447944d6c4ebacc5c83e5e9a8
sagar thakkar <sagarthakkar@sagars-MacBook-Air.local>
2025-07-23 09:58:05 +0530

    first problem solved

--- 01-twoSum.js
+++ 01-twoSum.js
@@ -1,10 +1,11 @@
 /**
- * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * Given an array of integers nums and an integer target, 
+ * return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You may assume that each input would have exactly one solution, and you may not use the same 
+element twice.
 
 You can return the answer in any order.
-
  
 
 Example 1:
@@ -21,3 +22,25 @@ Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
  */
+
+
+function twoSum(arr, target) {
+    const seen = new Map();
+    for (let i = 0; i < arr.length; i++) {
+        const ele = target - arr[i];
+        if (seen.has(ele)) {
+            return [seen.get(ele), i]
+        } else {
+            seen.set(arr[i], i);
+        }
+    }
+    return [];
+}
+
+
+
+// const arr = [2, 7, 11, 15]
+const arr=[3,3]
+const target = 6;
+const result = twoSum(arr, target);
+console.log({ result })
